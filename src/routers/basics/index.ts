404b21c321808da6054357f47647c9138bee1d6a
朽木白 <1547702880@qq.com>
2021-03-26 19:27:56 +0800

    feat：commit

--- src/routers/basics/index.ts
+++ src/routers/basics/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-26 14:59:55
- * @LastEditTime: 2021-03-26 15:08:51
+ * @LastEditTime: 2021-03-26 18:55:27
  * @LastEditors: Please set LastEditors
  * @Description: 基础路由，不需要权限
  * @FilePath: /vite-project/src/routers/basics/index.ts
