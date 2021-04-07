b60e4c4169228a8edd225448e92581e46b99f7b7
朽木白 <1547702880@qq.com>
2021-04-07 19:20:43 +0800

    feat：add less

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,15 +1,13 @@
 <!--
  * @Author: your name
  * @Date: 2021-03-26 10:03:54
- * @LastEditTime: 2021-04-07 16:25:23
+ * @LastEditTime: 2021-04-07 19:11:03
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vite-project/src/layouts/index.vue
 -->
 <template>
   <div>
-    Layout
-    <a-button type="primary">antd button</a-button>
     <router-view></router-view>
   </div>
 </template>
