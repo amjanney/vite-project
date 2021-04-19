7f8b30c6ef56a63f39e0a5bec4af7a8bb72f69eb
朽木白 <1547702880@qq.com>
2021-04-19 16:35:04 +0800

    feat：css 前缀名

---
+++ src/settings/designSetting.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-19 15:55:44
+ * @LastEditTime: 2021-04-19 16:30:26
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vite-project/src/settings/designSetting.ts
+ */
+
+// css 前缀名
+export default {
+  prefixCls: 'vite',
+};
