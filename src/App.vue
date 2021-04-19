15069b98c1133e59cd501f58239fb87fb6661d59
朽木白 <1547702880@qq.com>
2021-04-19 16:34:20 +0800

    feat：删除多余的样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-02-22 16:21:39
- * @LastEditTime: 2021-04-07 15:21:06
+ * @LastEditTime: 2021-04-16 17:53:33
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vite-project/src/App.vue
@@ -17,14 +17,3 @@ export default defineComponent({
   name: 'App'
 })
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
