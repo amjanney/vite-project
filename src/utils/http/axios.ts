fe2ffbf55204f25abe05abcff59a7179cb908dea
朽木白 <1547702880@qq.com>
2021-04-25 20:32:42 +0800

    feat：request

---
+++ src/utils/http/axios.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: janney
+ * @Date: 2021-04-25 20:19:56
+ * @LastEditTime: 2021-04-25 20:24:23
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vite-project/src/utils/http/axios.ts
+ */
+
+import axios from 'axios';
+
+
---
+++ src/utils/http/index.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-25 20:25:14
+ * @LastEditTime: 2021-04-25 20:26:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vite-project/src/utils/http/index.ts
+ */
+
+function createAxios() {
+
+}
+
+export const defHttp = createAxios();
